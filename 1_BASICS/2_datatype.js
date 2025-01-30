c4ae959278110ec502e24b0d28a847b26928aea8
Adarsh10-coder <btwadarsh1018@gmail.com>
2025-01-31 00:55:26 +0530

    datatype

---
+++ 1_BASICS/2_datatype.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) //not possible here bcoz we are using node js not browser
+
+console.log(3+3);
+
+let name ="Adarsh"
+let age = 19
+let isloggedin = false
+let state
+
+/* types of datatype
+  1.number => 2 to power 53
+  2.bigint
+  3.string => ""
+  4.boolean => true/false
+  5.null => standalone value
+  6.undefined =>
+  7.symbol => unique
+  8.object  
+  */
+
+console.log(typeof "Adarsh")
+console.log(typeof age)
+console.log(typeof null) // object
+console.log(typeof undefined) //undefined
+
+
+
+
