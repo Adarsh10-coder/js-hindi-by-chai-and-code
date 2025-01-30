62b98a5491518670a7f6c190c421d88c24f65003
Adarsh10-coder <btwadarsh1018@gmail.com>
2025-01-30 23:56:02 +0530

    variable

---
+++ 1_BASICS/1_variable.js
@@ -0,0 +1,18 @@
+const accountId = 123456
+let accountemail = "hlw@gmail.com"
+var accountpass = "78688559"
+accountcity = "jaipur"
+let accountstate;
+
+//accountId = 2 //not allowed
+accountemail ="abc@gamil.com"
+accountpass ="9857494"
+accountcity ="patna"
+
+console.log(accountId);
+/*
+prefer no to use var bcoz issue in block scope and function scope
+*/
+
+console.table([accountId,accountemail,accountpass,accountcity,accountstate])
+
