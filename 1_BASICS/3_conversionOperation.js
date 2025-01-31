8c829852907ed5a74ea6e1608cd0d7908d44440f
Adarsh10-coder <btwadarsh1018@gmail.com>
2025-01-31 22:58:04 +0530

    conversion

---
+++ 1_BASICS/3_conversionOperation.js
@@ -0,0 +1,15 @@
+let score ="33abc" // we can also use 24345,Null,undefined,true....etc
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isloggedin = 1 // we can also use "","Adarsh".....etc
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin);
+
+//Now we can convert another datatype similarly..
+
+
