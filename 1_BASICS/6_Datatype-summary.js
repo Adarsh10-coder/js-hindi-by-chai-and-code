c4ca5683321b9dbe676f002bf62b9fccde1f0242
Adarsh10-coder <btwadarsh1018@gmail.com>
2025-02-01 00:10:36 +0530

    Datatype summary

---
+++ 1_BASICS/6_Datatype-summary.js
@@ -0,0 +1,24 @@
+/* Types of datatype ---->
+1.primitive
+    String,Number,Boolearn,null,undefined,Symbol,BigInt
+
+2.Non-primitive(reference)   
+    Array,Objects,Functions
+*/
+
+const Id = Symbol('1234')
+const anotherId = Symbol('1234')
+console.log(Id===anotherId);
+
+const bigNumber = 85763876897996n
+
+const heros = ["ironman","thor","spiderman","hulk"];
+let myObj = {
+    name:"Adarsh",
+    age: 19,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+console.log(typeof myFunction);
