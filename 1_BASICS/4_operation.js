9faace3e7aba35e75b3e8118dccb008497443915
Adarsh10-coder <btwadarsh1018@gmail.com>
2025-01-31 23:22:48 +0530

    Operation

---
+++ 1_BASICS/4_operation.js
@@ -0,0 +1,28 @@
+let value = 3
+let negativeValue = -value
+console.log(negativeValue);
+
+/* Some operation are --->
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2); --it mean 2 to the power 2
+console.log(2%2);
+console.log(2/2);
+*/
+
+let str1 ="Hello"
+let str2 =" Adarsh"
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2); // If string is in begining then all input treat as string.
+console.log(1+2+"2");
+
+console.log((3+4)*5%3);
+
+let gameCounter = 100
+gameCounter++ // or ++gameCounter
+console.log(gameCounter);
