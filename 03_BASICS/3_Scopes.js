6802d0d3815a74e5505176722a9a22bef3248388
Adarsh10-coder <btwadarsh1018@gmail.com>
2025-03-14 19:44:32 +0530

    scope done

--- 03_BASICS/3_Scopes.js
+++ 03_BASICS/3_Scopes.js
@@ -5,3 +5,41 @@ if(true){
     console.log("INNER:",a);   
 }
 console.log(a);
+// console.log(b);
+
+function one(){
+    const username = "Adarsh"
+
+    function two (){
+        const website = "Youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}  
+// fuction two can oparate function one but function one can't operate two, so... console.log(website) doesn't oparate
+
+if (true){
+    const username = "Adarsh"
+    if(username==="Adarsh"){
+        const website = " youtube"
+        console.log(username + website);
+    }
+    // console.log(website);     It can't operate
+}  
+// console.log(username);     It can't operate
+
+
+// +++++++++++++++++++++++++ INTERESTING +++++++++++++++++++++++++++++++
+
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+/* addTwo(6)        ( It can't oparate)
+     const addTwo = function(num){               
+     return num + 2
+}
+*/
